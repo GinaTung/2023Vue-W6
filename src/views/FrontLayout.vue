b1c63f76668328e41e83075976ed4d1c5fe64473
董聿凌 <gb2013429@gmail.com>
2023-03-04 01:30:36 +0800

    update2

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -3,7 +3,7 @@
   <router-link to="/">Home</router-link> |
   <router-link to="/about">About</router-link> |
   <router-link to="/products">前台產品列表</router-link> |
-  <router-link to="/cart">前台購物</router-link>
+  <router-link to="/cart">前台購物車</router-link>
   <hr>
   <RouterView></RouterView>
 </template>
